d4b3f4a94877abd20c1120d53eab556e6ca34624
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-12-16 03:13:09 -0300

    cambios generales home/layout

--- src/Components/DescriptionHome.jsx
+++ src/Components/DescriptionHome.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Fade } from 'react-awesome-reveal'
+import { BoxIconElement } from 'boxicons';
 export default function DescriptionHome(props) {
-  let { name, photo } = props;
+  let { name, photo,rate } = props;
 
   return (
     <>
-<Fade className="snip1577">
-  <img src={photo} alt={name} className="w-full h-full"/>
-  <figcaption>
-    <h3>{name}</h3>
-    <h4>{name}</h4>
-  </figcaption>
-  <a href="#">Details</a>
+<Fade >
+    <div className='containerPhoto'>
+      <figure>
+      <img src={photo} alt={name} style={{height:220,width:330}}  />
+      <div className="capa">
+        <h3>{name}</h3>
+        <p><box-icon name='star' type='solid' flip='horizontal' animation='tada' ></box-icon>{rate}</p>
+      </div>
+      </figure>
+    </div>
 </Fade>
     </>
   );
--- src/Components/PhotoHome.jsx
+++ src/Components/PhotoHome.jsx
@@ -10,7 +10,7 @@ export default function PhotoHome(props) {
     <>
       <div className="imgDiv">
         <Fade>
-          <img src={photo} style={{height:200,width:300}} alt="game"></img>
+          <img src={photo} style={{height:220,width:280}} alt={name}></img>
           
         </Fade>
         

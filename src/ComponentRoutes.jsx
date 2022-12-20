da1b03601f479a2c0e4571671122330d30d901ae
cornicheli <gabrielcornide.pg@gmail.com>
2022-12-20 00:59:31 -0300

    layout librery

--- src/ComponentRoutes.jsx
+++ src/ComponentRoutes.jsx
@@ -6,6 +6,7 @@ import Home from "./Pages/Home";
 import Profile from "./Components/Profile/Profile";
 import Store from "./Pages/Store/Store";
 import ProfileEdit from "./Components/Profile/ProfileEdit";
+import Library from "./Pages/LibraryPage/Library";
 
 export default function ComponentRoutes() {
   return (
@@ -16,6 +17,7 @@ export default function ComponentRoutes() {
       <Route path="/Profile" element={<Profile />} />
       <Route path="/ProfileEdit" element={<ProfileEdit />} />
       <Route path="/Store" element={<Store />} />
+      <Route path="/Library" element={<Library />} />
     </Routes>
   );
 }

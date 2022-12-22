b35a0898ad42185f15fd7c50ba907b9785aadbe6
pungitoregerman <pungitoregerman@gmail.com>
2022-12-22 14:35:39 -0300

    Proteccion rutas y arreglado nav

--- src/ComponentRoutes.jsx
+++ src/ComponentRoutes.jsx
@@ -1,30 +1,49 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import SignInUp from "./Pages/SignInUp";
-import AddGame from "./Pages/AddGame";
-import Home from "./Pages/Home";
-import Cart from "./Pages/Cart/Cart";
+import SignInUp from "./pages/SignInUp";
+import AddGame from "./pages/AddGame";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart/Cart";
 import Profile from "./Components/Profile/Profile";
-import Store from "./Pages/Store/Store";
-import NotFoundPage from "./Pages/NotFound";
+import Store from "./pages/Store/Store";
+import NotFoundPage from "./pages/NotFound";
 import ProfileEdit from "./Components/Profile/ProfileEdit";
-import Library from "./Pages/LibraryPage/Library";
-import GameDitails from "./Pages/Ditails/GameDitails";
+import Library from "./pages/LibraryPage/Library";
+import GameDitails from "./pages/Ditails/GameDitails";
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 export default function ComponentRoutes() {
+
+  let user = useSelector((store) => store.userReducer);
+  let admin = user.role === 'admin'
+  let logged = user.logged 
+  
+
+
   return (
-    <Routes>
+      <Routes>
+      {/*  NoLogin-Login  */}
       <Route path="/" element={<Home />} />
       <Route path="/*" element={<NotFoundPage />} />
       <Route path="/SignInUp" element={<SignInUp />} />
-      <Route path="/AddGame" element={<AddGame />} />
-      <Route path="/Cart" element={<Cart />} />
       <Route path="/Ditails/:id" element={<GameDitails />} />
-
+      <Route path="/Store" element={<Store />} />
+       {/* End NoLogin-Login  */}
+      {/* {/* Users Login */}
+       <Route element={<ProtectedRoute isAllowed={!!logged} reDirect={"/SignInUp"} />}>
+      <Route path="/Cart" element={<Cart />} />
       <Route path="/Profile" element={<Profile />} />
       <Route path="/ProfileEdit" element={<ProfileEdit />} />
-      <Route path="/Store" element={<Store />} />
       <Route path="/Library" element={<Library />} />
+        </Route> 
+      {/* End Users Login */}
+      {/*  Admin */}
+      <Route element={<ProtectedRoute isAllowed={!!admin} reDirect="/" />}>
+          <Route path="/AddGame" element={<AddGame />} />
+       </Route>
+      {/*  End Admin */}
+    
     </Routes>
   );
 }

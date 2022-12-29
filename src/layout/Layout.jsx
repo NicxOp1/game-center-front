129b7647283b8eac8b627b54b69b92e572b5aa0f
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-12-29 20:10:24 -0300

    ..

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./header/Header";
-import { Footer } from "./Footer/Footer";
+import { Footer } from "./footer/Footer";
 
 export default function Layout(props) {
   return (
